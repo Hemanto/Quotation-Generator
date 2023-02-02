c5a7372710dee6f5b8e19af31135a5ce95890dc2
Hemanto <mehemanto@gmail.com>
2023-02-02 19:32:39 +0530

    Api url fix

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ async function getQuotes(params) {
   const api =
     "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
-    const response = await fetch(proxy + api);
-    const data = await response.json;
+    const response = await fetch(api);
+    const data = await response.json();
     console.log(data);
   } catch (error) {
     console.log("whoo", error);
