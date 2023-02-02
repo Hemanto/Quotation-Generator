ad92e366ca242e369d87f26fde8dcaf5218ed1ce
Hemanto <mehemanto@gmail.com>
2023-02-02 23:02:54 +0530

    active twitter button

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const quotAuthor = document.getElementById("quoteAuthor");
 console.log(quotAuthor);
 
 async function getQuotes() {
-  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const proxy = "https://www.sitelike.org/similar/cors-anywhere.herokuapp.com/";
   const api =
     "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
@@ -22,4 +22,13 @@ async function getQuotes() {
   }
 }
 
+function tweetQuote() {
+  console.log("clcik");
+  const quote = quotationText.innerText;
+  const author = quotAuthor.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+}
+newQutBtn.addEventListener("click", getQuotes);
+btnTwitter.addEventListener("click", tweetQuote);
 getQuotes();
