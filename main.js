238b6f84e891bd2e07fb0228c02bf998b0b953ef
Hemanto <mehemanto@gmail.com>
2023-02-02 20:16:07 +0530

    dom not working

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-async function getQuotes(params) {
+const quotationContainer = document.getElementById("quotationContainer");
+const quotationText = document.getElementById("quotationText");
+
+const btnTwitter = document.getElementById("btnTwitter");
+const newQutBtn = document.getElementById("newQutBtn");
+
+const quotAuthor = document.getElementById("quoteAuthor");
+console.log(quotAuthor);
+
+async function getQuotes() {
   const proxy = "https://cors-anywhere.herokuapp.com/";
   const api =
     "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
     const response = await fetch(api);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    quotAuthor.innerText = heelo;
   } catch (error) {
-    console.log("whoo", error);
+    getQuotes();
   }
 }
 
